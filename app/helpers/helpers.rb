c0cfa3a3372c4a82f2cac5a3ee185360a76f84af
Molly McCarron <mccarronmollye@gmail.com>
2019-01-27 10:09:03 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+
+  def self.current_user(session)#represents the hash
+    @user = User.find_by_id(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)# represents the hash
+    !! (session[:user_id])#if session hash has a user_id. #!! double negative and letting there be a true/false outcome. a single ! what be looking for the negation of, but since we want to verify double !! does the trick.
+  end
 end
